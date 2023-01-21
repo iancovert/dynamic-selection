d488f1028f5e49ac721bc656479c9c85466e455f
Ian Covert <icovert@cs.washington.edu>
2023-01-20 17:48:12 -0800

    Added more baselines to run.sh

--- experiments/mnist/run.sh
+++ experiments/mnist/run.sh
@@ -3,8 +3,7 @@ GPU=4
 NUM_RESTARTS=3
 NUM_TRIALS=5
 
-# for METHOD in sage permutation deeplift intgrad cae greedy
-for METHOD in intgrad cae greedy
+for METHOD in sage permutation deeplift intgrad cae greedy
 do
     python run.py --method=$METHOD --gpu=$GPU --num_trials=$NUM_TRIALS --num_restarts=$NUM_RESTARTS
 done
